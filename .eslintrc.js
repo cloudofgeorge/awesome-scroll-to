075f59c20457ef18e2ba26e9c8f6c0676081976d
Georgii Koniushkov <cloudofgeorge@gmail.com>
2020-02-19 15:47:42 +0300

    Setup Package

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+	parser: '@typescript-eslint/parser',
+	extends: [
+		'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+		'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+		'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+	],
+	env: {
+		browser: true,
+		es6: true,
+		jest: true,
+	},
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly',
+	},
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 2018,
+		sourceType: 'module',
+	},
+	rules: {
+		// Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+		// e.g. "@typescript-eslint/explicit-function-return-type": "off",
+		'prettier/prettier': 'error',
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'@typescript-eslint/explicit-member-accessibility': 'off',
+		'@typescript-eslint/no-non-null-assertion': 'off',
+		'@typescript-eslint/no-var-requires': 'off',
+	},
+	settings: {},
+};
